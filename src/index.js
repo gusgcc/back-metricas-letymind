a7437a788f2893cb24c210fc1af1fa975769fd4b
Gustavo Condezo Camayo <gustavocondezocamayo@gmail.com>
2020-12-16 23:32:06 -0500

    aa

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const express = require("express");
 const config = require("./server/config");
 //le pasamos el app de express() y nos devuelve un app configurado
 const app = config(express());
-
+app.set('port', process.env.PORT || 3000)
 app.listen(app.get("port"), () => {
     console.log("servidor en : ", app.get("port"))
 })
